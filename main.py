f0581a63a24d3cfe438f9a65d465838afc57cdc5
sule <sule.yavuz.905@gmail.com>
2025-06-19 18:09:49 +0300

    Implement Butterfly class: load sprite, set physics, and define initial state

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ FPS = 60
 CLOUD_SPAWN_INTERVAL = 2
 LEVEL_DURATION = 30
 GRAVITY_DELAY = 3
+
+class Butterfly:
+    def __init__(self):
+        self.image = pygame.image.load("butterfly.png")
+        self.rect = self.image.get_rect(center=(100, HEIGHT // 2))
+        self.gravity = 0.5
+        self.jump_strength = -10
+        self.velocity = 0
+        self.gravity_active = False
+        self.start_time = time.time()
+
+    def jump(self):
+        if self.gravity_active:
+            self.velocity = self.jump_strength
+
+    def update(self):
+        if not self.gravity_active and time.time() - self.start_time >= GRAVITY_DELAY:
+            self.gravity_active = True
+
+        if self.gravity_active:
+            self.velocity += self.gravity
+            self.rect.y += self.velocity
+        else:
+            self.velocity = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
